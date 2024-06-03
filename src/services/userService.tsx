b89db569f9d56e2bec1dde76be6f9839c334bdd0
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2024-06-02 23:26:46 -0300

    login

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -1,9 +1,10 @@
+import axios from 'axios';
 import api from './api';
 
 const getCurrentUser = async () => {
     try {
         const token = localStorage.getItem('accessToken');
-        const response = await api.post('/api/user/Customer', {}, {
+        const response = await axios.post('/api/user/Customer', {}, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
@@ -17,7 +18,7 @@ const getCurrentUser = async () => {
 
 const login = async (email:string, password:string) => {
     try {
-        const response = await api.post(`/api/user/login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`);
+        const response = await axios.post(`/api/user/login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`);
         return response.data;  
     } catch (error) {
         console.error(error);
