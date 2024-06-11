1ce4f70177e4dc631fa67780bb2e1c2bc5603e8e
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2024-06-11 04:50:35 -0300

    ajusutes

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        //target: 'https://api.faculdadedalavanderia.com.br',
-        target: 'http://localhost:5000',
+        target: 'https://api.faculdadedalavanderia.com.br',
+        //target: 'http://localhost:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false, // Adicione isso se o servidor usar HTTPS com certificado auto-assinado
