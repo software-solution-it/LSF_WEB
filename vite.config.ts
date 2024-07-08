ca6f07252f66d7b5d2267785536c2951c72ee371
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2024-07-08 17:09:11 -0300

    chat

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-       // target: 'https://api.faculdadedelavanderia.com.br',
-        target: 'http://localhost:5000',
+        target: 'https://api.faculdadedelavanderia.com.br',
+       // target: 'http://localhost:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: true
